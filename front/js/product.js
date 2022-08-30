c39d5b6d530c3f7a347f73eb04504a4a472fef76
Emeric <emericbarnes@gmail.com>
2022-08-30 15:42:30 +0200

    affichage ok

--- front/js/product.js
+++ front/js/product.js
@@ -1,87 +1,26 @@
-// const product = window.location.search.split("?").join("");
-// console.log(product);
-
-// let productData = [];
-
-
-// const fetchProduct = async () => {
-//   await fetch("http://localhost:3000/api/products/${product}")
-//     .then((res) => res.json())
-//     .then((promise) => {
-//   console.log(promise);
-//     });
-// };
-
-//  let fetchProduct;
-
 const params = new URLSearchParams(window.location.search);
 const id = params.get("id")
 console.log(id);
 
-// const getProduct = async => {
-//    await fetch(`http://localhost:3000/api/products/${id}`)
-//     .then(res => res.json())
-//     .then((promise) => {
-//         getProduct = promise
-//     })
-// }
-
-let getProduct = {};
 
 const fetchKanap = async () => {
 fetch(`http://localhost:3000/api/products/${id}`)
 .then(res => res.json())
 .then((promise) => {
-    getProduct = promise
+    product = promise
+    
+    
+    let img = document.querySelector(".item__img");
+    img.innerHTML = `<img src="${product.imageUrl}" alt="${product.altTxt}">`
+    document.getElementById(`title`).textContent =  product.name;
+    document.getElementById('price').textContent = product.price;
+    document.getElementById('description').textContent = product.description;
+    // document.getElementById('colors').textContent = `${product.colors}`;
+    let color = document.getElementById("colors");
+    for (i = 0; i < product.colors.length; i++) {
+      color.innerHTML += `<option value="${product.colors[i]}">${product.colors[i]}</option>`;
+    }
 })};
 
 fetchKanap();
 
-document.getElementById('price').textContent = 400;
-
-// function displayProduct();
-//     document.getElementById('item').innerHTML += ` 
-//   <a href="./product.html?id=${product._id}">
-//           <article>
-//             <img src="${product.imageUrl}" alt="${product.altTxt}">
-//             <h3 class="productName">${product.name}</h3>
-//             <p class="productDescription">${product.description}</p>
-//           </article>
-//         </a> `
-
-
-
-
-
-// fetchKanap();
-
-// const displayProduct = async => {
-//     await fetchKanap();
-
-// }
-// document.getElementById(`title`).textContent =  product.name;
-
-
-// function getProduct(){
-//     const name = product.name;
-//     const altTxt = product.altTxt;
-//     const colors = product.colors;
-//     const description = product.description;
-//     const imageUrl = product.imageUrl;
-//     const price = product.price /100;
-//     const _id = product._id;
-    
-// }
-
-
-// let img = document.createElement("img");
-// img.append('product.imageUrl');
-// document.getElementById('price').textContent = 200;
-
-
-// document.getElementById('description').innerHTML = 'hello';
-// document.getElementById('price').textContent = fetchProduct.price / 100;
-// document.getElementById('description').textContent = id.description;
-
-    
-    // document.getElementsByClassName('item').innerHTML +=
