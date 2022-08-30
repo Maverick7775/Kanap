a031b9136dbcedd852ce517b21a9f007137f71ae
Emeric <emericbarnes@gmail.com>
2022-08-30 07:47:58 +0200

    script.js ok

--- front/js/script.js
+++ front/js/script.js
@@ -1,29 +1,29 @@
-
 let getProducts = [];
 
 const fetchKanap = async () => {
     await fetch("http://localhost:3000/api/products")
     .then((res) => res.json())
-    .then((data) => {
-        getProducts = data;
-        console.log(getProducts);
-    });
-};
-
-const displayProducts = async() => {
-    await fetchKanap();
-
-array.forEach(product of  => {
-    
-});
-    document.getElementById("items").innerHTML += `
-    <a href="./product.html?id=${product._id}">
-    <article>
-      <img src=".../product01.jpg" alt="Lorem ipsum dolor sit amet, Kanap name1">
-      <h3 class="productName">Kanap name1</h3>
-      <p class="productDescription">Dis enim malesuada risus sapien gravida nulla nisl arcu. Dis enim malesuada risus sapien gravida nulla nisl arcu.</p>
-    </article>
-  </a>`
-}
+    .then((promise) => {
+        getProducts = promise;
+      })};
 
 fetchKanap();
+
+const displayProdutcs = async () => {
+  await fetchKanap();
+
+getProducts.forEach(product => {
+    document.getElementById('items').innerHTML += ` 
+  <a href="./product.html?id=${product._id}">
+          <article>
+            <img src="${product.imageUrl}" alt="${product.altTxt}">
+            <h3 class="productName">${product.name}</h3>
+            <p class="productDescription">${product.description}</p>
+          </article>
+        </a> `
+});
+
+}
+
+displayProdutcs();
+
