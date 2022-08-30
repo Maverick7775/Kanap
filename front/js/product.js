14cf6f495174c03811e68d20b584e13a640d2e6b
Emeric <emericbarnes@gmail.com>
2022-08-30 08:57:10 +0200

    commit rdv

--- front/js/product.js
+++ front/js/product.js
@@ -18,7 +18,7 @@ const params = new URLSearchParams(window.location.search);
 const id = params.get("id")
 console.log(id);
 
-// const fetchKanap = async => {
+// const getProduct = async => {
 //    await fetch(`http://localhost:3000/api/products/${id}`)
 //     .then(res => res.json())
 //     .then((promise) => {
@@ -26,36 +26,57 @@ console.log(id);
 //     })
 // }
 
-    fetch(`http://localhost:3000/api/products/${id}`)
-    .then(res => res.json())
-    .then((promise) => {
-        getProduct = promise
-    })
+let getProduct = {};
+
+const fetchKanap = async () => {
+fetch(`http://localhost:3000/api/products/${id}`)
+.then(res => res.json())
+.then((promise) => {
+    getProduct = promise
+})};
+
+fetchKanap();
+
+document.getElementById('price').textContent = 400;
+
+// function displayProduct();
+//     document.getElementById('item').innerHTML += ` 
+//   <a href="./product.html?id=${product._id}">
+//           <article>
+//             <img src="${product.imageUrl}" alt="${product.altTxt}">
+//             <h3 class="productName">${product.name}</h3>
+//             <p class="productDescription">${product.description}</p>
+//           </article>
+//         </a> `
+
+
+
+
+
 // fetchKanap();
 
 // const displayProduct = async => {
 //     await fetchKanap();
+
+// }
 // document.getElementById(`title`).textContent =  product.name;
 
+
+// function getProduct(){
+//     const name = product.name;
+//     const altTxt = product.altTxt;
+//     const colors = product.colors;
+//     const description = product.description;
+//     const imageUrl = product.imageUrl;
+//     const price = product.price /100;
+//     const _id = product._id;
+    
 // }
 
 
-function getProduct(product){
-    const name = product.name;
-    const altTxt = product.altTxt;
-    const colors = product.colors;
-    const description = product.description;
-    const imageUrl = product.imageUrl;
-    const price = product.price /100;
-    const _id = product._id;
-   
-    return getProduct(product);
-}
-
-let img = document.createElement("img");
-img.append('product.imageUrl');
+// let img = document.createElement("img");
+// img.append('product.imageUrl');
 // document.getElementById('price').textContent = 200;
-// document.getElementById('price').innerHTML = 400;
 
 
 // document.getElementById('description').innerHTML = 'hello';
@@ -63,4 +84,4 @@ img.append('product.imageUrl');
 // document.getElementById('description').textContent = id.description;
 
     
-    // document.getElementsByClassName('item').innerHTML += 
+    // document.getElementsByClassName('item').innerHTML +=
