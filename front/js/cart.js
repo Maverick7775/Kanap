e1b8adf34f694116ee235f9b1baf693f3710dcf0
Emeric <emericbarnes@gmail.com>
2022-10-25 10:55:18 +0200

    Récup array en cours

--- front/js/cart.js
+++ front/js/cart.js
@@ -1,18 +1,25 @@
-// const data = {
-//     id: id,
-//     color: color,
-//     quantity: Number(quantity),
-//     price: price
-    
-//   }
-console.log("coucou");
-// let productInLocalStorage = JSON.parse(localStorage.getItem("data"));
-// console.log(productInLocalStorage);
 
-// function getCart(){
-//     let items = [];
-// items = JSON.parse(localStorage.getItem("data"));
-// return items
-// }
+console.log("coucou");
 
 let productInLocalStorage = JSON.parse(localStorage.getItem('data'));
+console.log(productInLocalStorage);
+const numberOfItems = productInLocalStorage.length;
+console.log(numberOfItems); 
+
+console.log(typeof(productInLocalStorage));
+
+// const cart = Object.keys(productInLocalStorage);
+const cart = [];
+console.log(cart);
+
+console.log(typeof(cart));
+// cart.for(let i = 0; i < numberOfItems; i++)
+
+const items = localStorage.getItem(localStorage.key)
+console.log(items)
+// function displayProducts ();
+
+const article = document.createElement("article")
+article.classList.add("cart_item")
+
+const image = document.createElement('img');
